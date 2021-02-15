ddda8ce1dc35f47dff19bd7f880ea4a0c1db19fe
Angel Wu <44008675+arawup@users.noreply.github.com>
2021-02-15 01:14:31 -0500

    alert

---
+++ index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    alert("This website is still in progress and some of the content are placeholders. Thank You for your patience.");    
+})
